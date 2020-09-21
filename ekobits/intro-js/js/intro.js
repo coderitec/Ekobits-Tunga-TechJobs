48300f7cf7c3bf0aa5bb8e7cc0f96732bb136693
Francis Enemuo <38217960+francenemuo@users.noreply.github.com>
2020-09-21 20:26:34 +0100

    part II solution to introduction to js

--- ekobits/intro-js/js/intro.js
+++ ekobits/intro-js/js/intro.js
@@ -37,3 +37,29 @@ it will display "Keep it up" in the Console
 /*
 we can't actually say "if(isLearning === true)" because it has already been assigned]
  */
+
+let firstvariable;
+let secondvariable = "";
+let thirdvariable = 1;
+let secretMessage = "Shh!";
+
+if(firstvariable){
+    console.log("first");
+} else if(firstvariable || secondvariable){
+    console.log("second");
+} else if(firstvariable || thirdvariable){
+    console.log("third");
+} else {
+    console.log("fourth");
+}
+/*1. this gives us "third" because it has a value unlike secondvariable*/
+/* 2. firstvariable value is 'undefined' */
+/** 3. Is the value of firstvariable a "truthy" value? Why?
+ * it is a falsy value because it is not true when compared to others
+ * 4. Is the value of secondvariable a "truthy" value? Why?
+ * no, it is a falsy since the answer is not that
+ * 5. Is the value of thirdvariable a "truthy" value? Why?
+ * yes, it is a truthy value since it is the answer
+ */
+
+
