74f6e4f9088c0e640af94b03676902099badb2cd
Francis Enemuo <38217960+francenemuo@users.noreply.github.com>
2020-09-21 18:40:32 +0100

    part I solution to introduction to js

---
+++ ekobits/intro-js/js/intro.js
@@ -0,0 +1,39 @@
+//PART 1
+
+2 == "2";
+//this returns TRUE, since it evaluates agreement in value
+
+2 === 2;
+// this returns TRUE since it evaluates agreement in type and value
+
+10 % 3;
+// this returns 1, it us a remainder operator
+
+10 % 3 === 1;
+// this returns TRUE as 1 the remainder is the same as 1
+
+true && false
+//returns false
+
+false || true
+//returns TRUE
+
+true || false;
+//returns TRUE
+
+
+// PART 2
+let isLearning = true;
+if(isLearning){
+    console.log("Keep it up!");
+} else {
+    console.log("Pretty sure you are learning....");
+}
+/*
+QUestion 2
+it will display "Keep it up" in the Console
+*/
+
+/*
+we can't actually say "if(isLearning === true)" because it has already been assigned]
+ */
